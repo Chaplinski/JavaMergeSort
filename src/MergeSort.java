4cf6af4392256a25f92802e6ca2d71c78c4d7060
Scott <scott.chaplinski@gmail.com>
2020-04-16 21:02:54 -0500

    working commit

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class MergeSort {
 
 
@@ -11,7 +16,7 @@ class MergeSort {
         // k-way merge of files
 
 
-        String[] array = {"Scoop", "Carla", "Shrimp", "Monkeys", "Castaway"};
+        String[] array = args;
         System.out.println("Initial Array: ");
         printArray(array);
 
@@ -20,10 +25,21 @@ class MergeSort {
         printArray(array);
     }
 
+    public String[] runClass(String[] args){
+        String[] array = args;
+//        System.out.println("Initial Array: ");
+//        printArray(array);
+//
+        array = mergeSort(array);
+//        System.out.println("Sorted Array: ");
+//        printArray(array);
+        return array;
+    }
 
 
 
-    private static void printArray(String[] array){
+
+    public static void printArray(String[] array){
 
         for (String s: array){
             System.out.print(s + " ");
@@ -105,7 +121,87 @@ class MergeSort {
 
     }
 
+    protected static void finalMerge(ArrayList<String> fileList){
 
+        long totalBufferSize = 8L * 1024 * 1024 * 1024;
+        ArrayList<String[]> bufferList = new ArrayList<>();
+
+        // buffer size divided by the list size, divided by the number of bytes per line of input
+        long inputFileBufferSize = totalBufferSize/fileList.size()/100;
+        System.out.println(inputFileBufferSize);
+
+
+        for (int i = 0; i < fileList.size(); i++){
+            //for each file create a buffer the size of 1/N * totalBufferSize
+            String[] buffer = new String[(int)inputFileBufferSize];
+
+            try {
+
+                //fill each buffer with x number of values from respective file
+                File file = new File(fileList.get(i));
+                Scanner scan = new Scanner(file);
+
+                while(scan.hasNextLine()) {
+                    buffer[i] = scan.nextLine();
+                }
+                bufferList.add(buffer);
+
+
+                } catch (IOException io){
+
+            }
+
+        }
+
+        //now all files have their initial buffer
+        // we will compare their buffer
+
+
+        //while input files still have contents
+
+            //while outputBuffer is not full
+                //get first value in each input file buffer
+
+                //compare all values and add the i lowest one to the buffer[i]
+
+                //get next value from file that just added
+
+            //when output buffer is full, write buffer to storage
+
+            //empty buffer
+
+
+
+//        String[] result = new String[left.length + right.length];
+//
+//        int leftPointer, rightPointer, resultPointer;
+//        leftPointer = rightPointer = resultPointer = 0;
+//
+//        while(leftPointer < left.length || rightPointer < right.length){
+//
+//            if(leftPointer < left.length && rightPointer < right.length){
+//
+//                //                if(left[leftPointer] < right[rightPointer]){
+//                if(left[leftPointer].compareTo(right[rightPointer]) < 0){
+//                    result[resultPointer++] = left[leftPointer++];
+//                } else {
+//                    result[resultPointer++] = right[rightPointer++];
+//                }
+//
+//            }
+//
+//            else if(leftPointer < left.length){
+//
+//                result[resultPointer++] = left[leftPointer++];
+//
+//            }
+//
+//            else if (rightPointer < right.length){
+//                result[resultPointer++] = right[rightPointer++];
+//            }
+//
+//        }
+    }
 }
 
 
--- src/OpenFile.java
+++ src/OpenFile.java
@@ -1,39 +1,71 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class OpenFile {
 
+
+
     public static void main(String args[]) throws FileNotFoundException, IOException {
 
-        int bufferSize = 40000000;
+
+        ArrayList<String> fileList = new ArrayList<>();
+
+        int bufferSize = 3000;
         File file = new File("testfile.txt");
         Scanner scan = new Scanner(file);
         String[] buffer = new String[bufferSize];
 
+
         String fileContent = "";
         int i = 0;
+        int file_num = 1;
+
+
+        //TODO add multithreading
         while(scan.hasNextLine()){
             buffer[i]= scan.nextLine();
-            //fileContent = fileContent.concat(scan.nextLine() + "\n");
-            if(i >= bufferSize){
-                //mergesort buffer
 
+            if((i >= bufferSize - 1) || (!scan.hasNextLine())){
+                //mergesort buffer
+//                System.out.println(bufferSize);
+
+                MergeSort ms = new MergeSort();
+                buffer = ms.runClass(buffer);
+
+                String filename = "testwrite" + file_num + ".txt";
                 //write sorted buffer to disk
+                Writer output = new BufferedWriter(new FileWriter(filename, true));
+
+                fileList.add(filename);
+                for(String s: buffer){
+                    if(s.compareTo("") != 0) {
+                        output.append(s);
+                        ((BufferedWriter) output).newLine();
+                    }
+                }
+                output.close();
+
+                //since we are incrementing i after this if statement we set it to negative 1,
+                // so it will be 0 the next time it is used
+                i = -1;
+                file_num++;
+                Arrays.fill(buffer, "");
 
                 //reset i to 0
-                break;
+
             }
 
             i++;
 
 
         }
-        System.out.println(buffer.length);
-        System.out.println(buffer[3]);
+        System.out.println("Total Number of output files: " + (file_num -1));
 
-//        FileWriter writer = new FileWriter("nexfile.txt");
-//        writer.write(fileContent);
-
+        // TODO implemented k-way external sort using one thread
+        MergeSort ms = new MergeSort();
+        ms.finalMerge(fileList);
 
     }
 }
